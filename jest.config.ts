2f2e20b23fab1eff3db720c9ea3b5fc6ac10fbb3
Duy Khanh <42001499+dnk22@users.noreply.github.com>
2024-11-07 15:24:25 +0700

    (source) : init

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'my-money-be',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/my-money-be',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
